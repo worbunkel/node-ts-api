c04a3d73b5260b99117257c4a3e2c4c94eb21bd0
Luke Brown <worbunkel@gmail.com>
2019-05-03 08:32:00 -0500

    Set port to 8080

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const port = 47502;
+const port = 8080;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
