95b6dc0a7fdea5e11071445673988ae7ac3ecac3
Luke Brown <worbunkel@gmail.com>
2019-05-29 09:19:21 -0500

    Add smart screen lenses to insurance plans

--- src/Benefit.ts
+++ src/Benefit.ts
@@ -8,6 +8,8 @@ export enum BenefitType {
   OPTOS_EYE_EXAMINATION = 'Optos Eye Examination',
   SPECTACLE_LENSES = 'Spectacle Lenses',
   FRAMES = 'Frames',
+  DELIVERY = 'Delivery',
+  SMART_SCREEN_LENSES = 'Smart Screen Lenses',
   SINGLE_VISION_LENSES = 'Single Vision Lenses',
   LINED_BIFOCAL_LENSES = 'Lined Bifocal Lenses',
   TRIFOCAL_LENSES = 'Trifocal Lenses',
